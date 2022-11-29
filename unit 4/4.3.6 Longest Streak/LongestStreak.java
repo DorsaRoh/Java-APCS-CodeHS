cfa654762a8257b9ddc50f11586b32a89159d8fb
Dorsa Rohani <85207154+DorsaRoh@users.noreply.github.com>
2022-11-29 00:02:49 -0500

    Create LongestStreak.java

---
+++ unit 4/4.3.6 Longest Streak/LongestStreak.java
@@ -0,0 +1,36 @@
+public class LongestStreak extends ConsoleProgram
+{
+    public static final int FLIPS = 100;
+    
+    public void run()
+    {
+        
+        int countHeads = 0, longestStreak = 0, streak = 0;
+        
+        for(int i = 0; i < FLIPS; i++)
+        {
+            
+            if(Randomizer.nextBoolean())
+            {
+                System.out.println("Heads");
+                streak ++;
+            }
+            else
+            {
+                System.out.println("Tails");
+                
+                if(longestStreak < streak)
+                {
+                    longestStreak = streak;
+                }
+                
+                streak = 0;
+            }
+            
+
+        }
+
+        System.out.println("Longest streak of heads: " + longestStreak);
+
+    }
+}
