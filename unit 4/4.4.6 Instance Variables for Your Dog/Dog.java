6b99430c5646e435404d334a2f53114bd40a857a
Dorsa Rohani <85207154+DorsaRoh@users.noreply.github.com>
2022-11-29 21:08:56 -0500

    Create Dog.java

---
+++ unit 4/4.4.6 Instance Variables for Your Dog/Dog.java
@@ -0,0 +1,16 @@
+public class Dog 
+{
+    private String breed;
+    private String name;
+    
+    public Dog(String theBreed, String theName)
+    {
+        breed = theBreed;
+        name = theName;
+    }
+    
+    public String toString()
+    {
+        return name + " is a " + breed;
+    }
+}
