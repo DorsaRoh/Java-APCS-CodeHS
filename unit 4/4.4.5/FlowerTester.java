7ea563b6fa73d2f04a86cdc52fe72d519cc29836
Dorsa Rohani <85207154+DorsaRoh@users.noreply.github.com>
2022-11-29 21:05:54 -0500

    Create FlowerTester.java

---
+++ unit 4/4.4.5/FlowerTester.java
@@ -0,0 +1,9 @@
+public class FlowerTester extends ConsoleProgram
+{
+    public void run()
+    {
+        Flower foxglove = new Flower("Foxglove", "Red", "Digitalis", "obscura");
+        System.out.println(foxglove);
+    }
+
+}
