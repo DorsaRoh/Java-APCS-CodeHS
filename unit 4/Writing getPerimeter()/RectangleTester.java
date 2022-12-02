8d926fa6596cce9ce8d7a086bfd9b4d3a57997a4
Dorsa Rohani <85207154+DorsaRoh@users.noreply.github.com>
2022-12-02 16:30:51 -0500

    Create RectangleTester.java

---
+++ unit 4/Writing getPerimeter()/RectangleTester.java
@@ -0,0 +1,8 @@
+public class RectangleTester extends ConsoleProgram
+{
+    public void run()
+    {
+        Rectangle r1 = new Rectangle(2, 5);
+        System.out.println(r1.getPerimeter());
+    }
+}
