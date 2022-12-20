d8943a2dd6372e54180c87391fc9ceef1d532c6d
Dorsa Rohani <85207154+DorsaRoh@users.noreply.github.com>
2022-12-20 14:22:18 -0500

    Create Clothing.java

---
+++ unit 4/4.13.4 Clothing Store/Clothing.java
@@ -0,0 +1,22 @@
+public class Clothing
+{
+    public String s;
+    public String c;
+    
+    public Clothing(String size, String color)
+    {
+        s = size;
+        c = color;
+    }
+    
+    public String getSize()
+    {
+        return s;
+    }
+    
+    public String getColor()
+    {
+        return c;
+    }
+    
+}
