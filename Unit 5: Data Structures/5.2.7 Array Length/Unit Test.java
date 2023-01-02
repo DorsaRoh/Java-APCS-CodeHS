e88d7e45108ab08b9b5aa6e9465e687323a78347
Dorsa Rohani <85207154+DorsaRoh@users.noreply.github.com>
2023-01-02 17:35:53 -0500

    Create Unit Test.java

---
+++ Unit 5: Data Structures/5.2.7 Array Length/Unit Test.java
@@ -0,0 +1,14 @@
+public class Scratchpad extends ConsoleProgram
+{
+    public void run()
+    {
+        int[] arr = new int[5];
+        arrayLength(arr);
+    }
+
+    public int arrayLength(int[] array)
+        {
+            System.out.println(array.length);
+            return array.length;
+        }
+}
